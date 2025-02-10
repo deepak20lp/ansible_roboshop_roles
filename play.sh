424a4518d93d52e441b5ab0b6202959f3dd6b6a6
deepak20lp <deepakreddy17kb@gmail.com>
2025-02-10 10:45:16 +0530

    secong commit

---
+++ play.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ansible-playbook -i inventory -e component=mongodb main.yml
+ansible-playbook -i inventory -e component=catalogue main.yml
+ansible-playbook -i inventory -e component=web main.yml
+ansible-playbook -i inventory -e component=redis main.yml
+ansible-playbook -i inventory -e component=user main.yml
+ansible-playbook -i inventory -e component=cart main.yml
+ansible-playbook -i inventory -e component=mysql main.yml
+ansible-playbook -i inventory -e component=shipping main.yml
+ansible-playbook -i inventory -e component=rabbitmq main.yml
+ansible-playbook -i inventory -e component=payment main.yml
